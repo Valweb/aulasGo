f5370cf4a299ce01939df641f74e80950398c79b
Valeria <valeriaweb@gmail.com>
2017-12-14 21:03:48 -0200

    Nova aula de switch

---
+++ Switch/main.go
@@ -0,0 +1,24 @@
+package main
+
+
+func main(){
+	numero := 3
+	fmt.print("O numero se esceve", numero,"se escreve assim")
+	switch numero{
+	case 1:
+		fmt.println("um.")
+	case 2:
+		se 1:
+		fmt.println("dois.")
+	case 3:
+		se 1:
+		fmt.println("tres.")
+	case 4:
+		se 1:
+		fmt.println("quatro.")
+	case 5:
+		se 1:
+		fmt.println("cinco .")
+	
+	}
+}
